1cc5e0f78d1c4047979359f8e9cc0f78baa4cb02
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2024-07-24 05:11:30 +0700

    update

--- src/main/java/th/mfu/boot/SecurityConfig.java
+++ src/main/java/th/mfu/boot/SecurityConfig.java
@@ -1,21 +1,42 @@
 package th.mfu.boot;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String AUTH_TOKEN_HEADER_NAME = "apikey";
+    private static final String AUTH_TOKEN = "123456";
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/api/public/**").permitAll() // Allow public access
-                .anyRequest().authenticated() // Require authentication for other requests
-                .and()
-                .csrf().disable() // Disable CSRF (http://www.baeldung.com/spring-security-csrf)
-            .httpBasic(); // Enable Basic Authentication
+       APIKeyAuthFilter filter = new APIKeyAuthFilter(AUTH_TOKEN_HEADER_NAME);
+        filter.setAuthenticationManager(new AuthenticationManager() {
+
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String principal = (String) authentication.getPrincipal();
+                if (!AUTH_TOKEN.equals(principal))
+                {
+                    throw new BadCredentialsException("The API key was not found or not the expected value.");
+                }
+                authentication.setAuthenticated(true);
+                return authentication;
+            }
+        });
+        http.
+            antMatcher("/secure/**").
+            csrf().disable().
+            sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+            and().addFilter(filter).authorizeRequests().anyRequest().authenticated();
     }
 }
